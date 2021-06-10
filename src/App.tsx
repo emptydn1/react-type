951f6227ae78a450bca0dd9d9c4bbbf4b0473b4c
huy <huy@gmail.com>
2021-06-10 21:20:14 +0700

    add redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+
 import styled from "styled-components";
 
 const Button = styled.button`
   color: red;
 `;
 
-function App() {
+const App: React.FC = () => {
+  const [state, setstate] = useState<string>("");
+
+  const a = () => {
+    setstate("");
+  };
+
   return (
     <div>
+      {state}
       {process.env.NAME}
       <Button>Primary</Button>
     </div>
   );
-}
+};
 
 export default App;
