ff635706c2391c9c0825d9e79d6d126760fc2e1e
huy <huy@gmail.com>
2021-06-11 23:51:47 +0700

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
 import React, { useState } from "react";
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
+import { Grid, Container, Paper, IconButton, Button, Typography, Toolbar, AppBar } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 
-import styled from "styled-components";
-
-const Button = styled.button`
-  color: red;
-`;
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(1),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+    red: {
+      backgroundColor: "red"
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }),
+);
 
 const App: React.FC = () => {
+  const classes = useStyles();
   const [state, setstate] = useState<string>("");
 
   const a = () => {
@@ -14,11 +34,30 @@ const App: React.FC = () => {
   };
 
   return (
-    <div>
-      {state}
-      {process.env.NAME}
-      <Button>Primary</Button>
-    </div>
+    <Container maxWidth="lg" className={classes.red}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      <Grid container spacing={1} >
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>item</Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>item</Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>item</Paper>
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
